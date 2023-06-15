60774fef8625029122c14f28d733017864a5e8e4
James Kennedy <acinea.kennedy@gmail.com>
2023-06-15 10:50:40 -0400

    first commit

---
+++ Js/message-generator.js
@@ -0,0 +1,5 @@
+const messages = ["You can do it!", "Today is Tomorrow's Yesterday!", "Please try again", "Da fuq??"]
+const randomNumber = Math.floor(Math.random() * messages.length)
+
+console.log('hello')
+console.log(randomNumber)
